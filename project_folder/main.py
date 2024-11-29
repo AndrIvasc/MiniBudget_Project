f5501b565fd7f9583ba903183b8765828bb87e3a
AndrIvasc <andrejus.iv1880@gmail.com>
2024-11-29 10:45:29 +0200

    Added simple menu functionality. Added user input simple functionality

--- project_folder/main.py
+++ project_folder/main.py
@@ -1,1 +1,29 @@
-print("hello world")
+# Main program and menu selection
+
+while True:
+
+    print("1. Enter income\n"
+          "2. Enter expenses\n"
+          "3. Print income rows\n"
+          "4. Print expense rows\n"
+          "5. Print statistics\n"
+          "q - Exit programm\n"
+          "Enter your choice:")
+
+    user_input = input("> ")
+
+    if user_input == '1':
+        input("Press enter to continue...")
+    elif user_input == '2':
+        input("Press enter to continue...")
+    elif user_input == '3':
+        input("Press enter to continue...")
+    elif user_input == '4':
+        input("Press enter to continue...")
+    elif user_input == '5':
+        input("Press enter to continue...")
+    elif user_input == 'q':
+        print("Exiting the program. Goodbye!")
+        break
+    else:
+        print("Invalid choice. Please try again.")
